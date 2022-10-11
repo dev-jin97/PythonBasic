c21cc46f1b99357b8daebd6ff9097e888137e6c4
Hyeonjin Kim <guswls0975@naver.com>
2022-10-11 12:56:27 +0900

    update : 입력과 자료형 변환 datetime 함수 활용

--- Chapter4/code/03.입력,자료형변환.py
+++ Chapter4/code/03.입력,자료형변환.py
@@ -9,3 +9,24 @@ add = int(x) + int(y) # 자료형 변환을 안했을 시 2040, int형 변환시
 str_add = x + y # 자료형 변환을 안했을 시 2040, int형 변환시 60
 
 print(add, str_add)
+
+# 사용자로부터 태어난 연도를 입력받으면, 현재 나이를 출력하기
+
+'''
+표준입력 
+태어난 연도를 입력하세요 >>> 1994 
+
+표준출력
+현재나이는 28세입니다.
+'''
+import datetime
+
+dt_now = datetime.datetime.now()
+
+current_year = dt_now.year
+
+age_calculator = input("태어난 연도를 입력하세요 >>> ")
+
+current_age = current_year - int(age_calculator)
+
+print("현재 나이는 만{}세입니다.".format(current_age))
